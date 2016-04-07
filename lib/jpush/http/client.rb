931e7e61036702890af6e4eb87074567e98e70df
Helperhaps <helperhaps@gmail.com>
2016-04-06 22:53:25 -0700

    add modified headers to http client

--- lib/jpush/http/client.rb
+++ lib/jpush/http/client.rb
@@ -5,21 +5,22 @@ module Jpush
     module Client
       extend self
 
-      def get(url, params: nil)
-        send_request(:get, url, params: params)
+      def get(url, params: nil, headers: {})
+        send_request(:get, url, params: params, headers: headers)
       end
 
-      def post(url, body:)
-        send_request(:post, url, body: body)
+      def post(url, body: , headers: {})
+        send_request(:post, url, body: body, headers: headers)
       end
 
-      def delete(url, params: nil)
-        send_request(:delete, url, params: params)
+      def delete(url, params: nil, headers: {})
+        send_request(:delete, url, params: params, headers: headers)
       end
 
-      def send_request(method, url, params: nil, body: nil)
+      def send_request(method, url, params: nil, body: nil, headers: {})
+        headers = jpush_headers.merge(headers)
         raw_response =
-          Utils::Http.new(method.to_sym, url, params: params, body: body).
+          Utils::Http.new(method.to_sym, url, params: params, body: body, headers: headers).
           basic_auth(jpush_basic_auth[:user], jpush_basic_auth[:pass]).
           send_request
 
@@ -41,7 +42,10 @@ module Jpush
 
         def jpush_headers
           {
-            accept: 'application/json'
+            'user-agent' => 'jpush-ruby-sdk-client/' + Jpush::VERSION,
+            'accept' => 'application/json',
+            'content-type' => 'application/json',
+            'connection' => 'close'
           }
         end
 
--- lib/jpush/utils/http.rb
+++ lib/jpush/utils/http.rb
@@ -17,7 +17,7 @@ module Jpush
         raise Utils::Exceptions::JpushError, err_msg unless HTTP_VERB_MAP.keys.include?(method)
         @uri = URI(url)
         @uri.query = URI.encode_www_form(params) unless params.nil?
-        @request = prepare_request(method, body)
+        @request = prepare_request(method, body, headers)
       end
 
       def send_request
@@ -33,8 +33,9 @@ module Jpush
 
       private
 
-        def prepare_request(method, body)
+        def prepare_request(method, body, headers)
           request = HTTP_VERB_MAP[method].new @uri
+          request.initialize_http_header(headers)
           request.body = body.to_json unless body.nil?
           request
         end
