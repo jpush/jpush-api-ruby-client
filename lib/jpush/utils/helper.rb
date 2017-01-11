745f64d6143b9b79c468a26dbbb1aa9deeee3771
Helperhaps <helperhaps@gmail.com>
2017-01-11 08:42:04 +0000

    remove blank?

--- lib/jpush/utils/helper.rb
+++ lib/jpush/utils/helper.rb
@@ -3,33 +3,6 @@ module JPush
     module Helper
       module ObjectExtensions
 
-        refine Object do
-
-          # An object is blank if it's false, empty, or a whitespace string.
-          # For example, false, '', ' ', nil, [], and {} are all blank.
-          def blank?
-            respond_to?(:empty?) ? !!empty? : !self
-          end
-
-        end
-
-        refine String do
-          BLANK_RE = /\A[[:space:]]*\z/
-          WORD_RE = /\A[\p{han}a-zA-Z0-9_@!#&=|\$\*\+\.]+\z/u
-
-          def titleize
-            self.split(' ').map(&:capitalize).join(' ')
-          end
-
-          def blank?
-            BLANK_RE === self
-          end
-
-          def valid_word?
-            WORD_RE === self
-          end
-        end
-
         refine Hash do
           def compact
             self.select { |_, value| !value.nil? }
