8326e3eb55f571619ed2fe49cd14392ea08307bc
Helperhaps <helperhaps@gmail.com>
2017-01-11 07:09:39 +0000

    remove jpush_symbolize_keys!

--- lib/jpush/utils/helper.rb
+++ lib/jpush/utils/helper.rb
@@ -5,12 +5,6 @@ module JPush
 
         refine Object do
 
-          def jpush_symbolize_keys!
-            return self.inject({}){ |memo, (k, v)| memo[k.to_sym] = v.jpush_symbolize_keys!; memo } if self.is_a? Hash
-            return self.inject([]){ |memo, v     | memo          << v.jpush_symbolize_keys!; memo } if self.is_a? Array
-            return self
-          end
-
           # An object is blank if it's false, empty, or a whitespace string.
           # For example, false, '', ' ', nil, [], and {} are all blank.
           def blank?
